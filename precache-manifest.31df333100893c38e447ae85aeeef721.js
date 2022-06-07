dc2ee8fd996722ce101e135d8245c16c7dc65693
Mac book pro Johann <macbookprojohann@MacBook-Pro-de-Mac.local>
2022-06-07 10:53:25 +0200

    Updates

---
+++ precache-manifest.31df333100893c38e447ae85aeeef721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14a9b682cc15c7695b1b",
+    "url": "/ProjectReport/static/css/main.1286dc5a.chunk.css"
+  },
+  {
+    "revision": "14a9b682cc15c7695b1b",
+    "url": "/ProjectReport/static/js/main.14a9b682.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/ProjectReport/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "c3bafbef525e5bb6c8c3",
+    "url": "/ProjectReport/static/js/2.c3bafbef.chunk.js"
+  },
+  {
+    "revision": "61e34eb8630620015a8a",
+    "url": "/ProjectReport/static/js/runtime~main.61e34eb8.js"
+  },
+  {
+    "revision": "9fffa3db2cc86a41c9d5191bba0b517b",
+    "url": "/ProjectReport/index.html"
+  }
+];
